3d46e5ac8808d670cba2e42c87734bce875f8787
MoeMoney07 <72325422+MoeMoney07@users.noreply.github.com>
2025-05-07 01:38:12 +0000

    lab 3

--- Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731561dd59005d474405ef84b4881cdb36dac6ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9fa6e720ebd8fb0841cdfdcafb9ead00b4e827")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
